99ce5474d47294377502f955f097a1c3e1ee97a8
longjinwen <dxfjyygy@sina.com>
2017-03-15 12:02:16 +0800

    sdf

--- src/main/java/com/dxfjyygy/test/test1/TestDI1.java
+++ src/main/java/com/dxfjyygy/test/test1/TestDI1.java
@@ -7,9 +7,13 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  * Created by longjinwen on 2017/2/28.
  */
 public class TestDI1 {
-    public static void main(String[] args) {
-//        ApplicationContext ctx = new ClassPathXmlApplicationContext("classpath:/WEB/INF/mvc-dispatcher-servlet.xml");
-//        Person p =ctx.getBean("chinese",Person.class);
-//        p.useAxe();
-    }
+//    public static void main(String[] args) {
+////        ApplicationContext ctx = new ClassPathXmlApplicationContext("classpath:/WEB/INF/mvc-dispatcher-servlet.xml");
+////        Person p =ctx.getBean("chinese",Person.class);
+////        p.useAxe();
+//        System.out.println("abc");
+//    }
+        public static void main(String[] args) {
+            System.out.println("hello");
+        }
 }
